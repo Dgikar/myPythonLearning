d8ea93044bc0055bdb7bd900b607aa12a48f94a2
Dgikar <dgikar@ukr.net>
2023-01-11 23:42:12 +0200

    Python - OOP - Lesson - 07 - Homeworks

--- 02 - Python - OOP/Python - OOP - Lesson - 07/Python - OOP - Homeworks/Python - OOP - Lesson - 07 - Homeworks.py
+++ 02 - Python - OOP/Python - OOP - Lesson - 07/Python - OOP - Homeworks/Python - OOP - Lesson - 07 - Homeworks.py
@@ -6,90 +6,114 @@ Useful Links:
 -------------------------------------------
 
 1. Реалізуйте генераторну функцію, яка повертатиме по одному члену геометричної прогресії із зазначеним множником.
-    Генератор повинен зупинити свою роботу або після досягнення зазначеного елементу, або при передачі команди на завершення.
+    Генератор повинен зупинити свою роботу або після досягнення зазначеного елементу, або при передачі команди на
+     завершення.
 """
 
+
+def geometric_progression(denominator: int, stop: int):
+    """ Генератор геометричної прогресії """
+
+    start = 1
+
+    while start * denominator <= stop:
+        yield f"{start} * {denominator} = {start * denominator}"
+        start += 1
+
+    return
+
+
+print("-" * 42)
+print("Генератор геометричної прогресії:")
+for i in geometric_progression(7, 70):
+    print(f"    {i}")
+
+print("-" * 35)
 # -------------------------------------------
 
 """
 2. Реалізуйте свій аналог генераторної функції range()
 """
 
+
+def range_func(*args, **kwargs):
+    """ Кастомна функція range() """
+
+    if not args or len(args) > 3:
+        raise TypeError
+
+    start = args[0] if len(args) >= 2 else 0
+    stop = args[1] if len(args) >= 2 else args[0]
+    step = args[2] if len(args) == 3 else 1
+
+    if not step:
+        raise ValueError
+
+    if start < 0 and stop > start:
+        return
+
+    if step > 0 and stop < start:
+        return
+
+    while start < stop:
+        yield start
+        start += step
+
+    return
+
+
+range1 = range_func(4, 20, 2)
+
+print("Кастомна функція range():")
+for i in range1:
+    print(f"    {i}")
+
+print("-" * 35)
 # -------------------------------------------
 
 """
-3. Напишіть функцію-генератор, яка повертатиме прості числа. Верхня межа діапазону повинна бути задана параметром цієї функції.
+3. Напишіть функцію-генератор, яка повертатиме прості числа.
+    Верхня межа діапазону повинна бути задана параметром цієї функції.
 """
 
+
+def prime_numbers(multiplier_args):
+    """ Функція-генератор простих чисел """
+
+    start = 2
+
+    while start <= multiplier_args:
+        prime_numbers_list = [1 for i in range(2, start + 1) if start % i == 0]
+
+        if len(prime_numbers_list) > 1:
+            start += 1
+            continue
+
+        else:
+            yield start
+
+        start += 1
+
+    return
+
+
+print("Функція-генератор простих чисел:")
+for i in prime_numbers(25):
+    print(f"    {i}")
+
+print("-" * 35)
 # -------------------------------------------
 
 """
-4. Напишіть генераторний вираз для заповнення списку. Список повинен бути заповнений кубами чисел від 2 до вказаної вами величини.
+4. Напишіть генераторний вираз для заповнення списку.
+    Список повинен бути заповнений кубами чисел від 2 до вказаної вами величини.
 """
 
-# -------------------------------------------
 
-"""
-Додаткові завдання:
-    1. Create a class that performs statistical processing of a text file - counting characters, words, sentences, etc.
-        Determine the required attributes-data and attributes-methods in class for working with the text file.
-        
-        Створити клас, який виконує статистичну обробку текстового файлу – підрахунок символів, слів, речень тощо.
-        Визначити необхідні атрибути-дані та атрибути-методи в класі для роботи з текстовим файлом.
-"""
+def numbers_list(entry_args):
+    """ Генератор від 2 до вказаної величини """
 
-# -------------------------------------------
+    return (i ** 3 for i in range(2, entry_args))
 
-"""
-    2. Pizzeria offers pizza-of-the-day for business lunch. The type of pizza of the day depends on the day of the week.
-        Having a pizza-of-the-day simplifies ordering for customers. They don't have to be experts on specific types of
-        pizza. Also, customers can add extra ingredients to the pizza of the day. Write a program that will form orders
-        from customers.
-        
-        Піцерія пропонує піцу дня для бізнес-ланчу. Вид піци дня залежить від дня тижня. Наявність піци дня спрощує
-        замовлення для клієнтів. Їм не обов’язково бути експертами з конкретних видів піци. Крім того, клієнти можуть
-        додати додаткові інгредієнти до піци дня. Написати програму, яка буде формувати замовлення від клієнтів.
-"""
 
-# -------------------------------------------
-
-"""
-    3. Write a program for selling tickets to IT events. Each ticket has a unique number and a price.
-        There are four types of tickets:
-            - regular ticket
-            - advance ticket (purchased 60 or more days before the event)
-            - late ticket (purchased fewer than 10 days before the event)
-            - and student ticket.
-            
-            Additional information:
-                - advance ticket - discount of 40% of the regular ticket price;
-				- student ticket - discount of 50% of the regular ticket price;
-				- late ticket - additional 10% to the regular ticket price.
-			
-			    All tickets must have the following properties:
-				    - the ability to construct a ticket by number;
-				    - the ability to ask for a ticket’s price;
-				    - the ability to print a ticket as a String.
-        
-        Для останньої задачі реалізувати серіалізацію / десеріалізацію у форматі json проданих квитків.
-            
-    
-        Написати програму продажу квитків на IT-заходи. Кожен квиток має унікальний номер і ціну. Існує чотири типи
-        квитків:
-            - звичайний квиток
-            - попередній квиток (куплений за 60 або більше днів до події)
-            - пізній квиток (куплений менш ніж за 10 днів до події)
-            - та студентський квиток.
-            
-            Додаткова інформація:
-                - попередній квиток - знижка 40% від звичайної вартості квитка;
-                - студентський квиток - знижка 50% від звичайної вартості квитка;
-                - пізній квиток - додатково 10% до звичайної вартості квитка.
-                
-                Усі квитки повинні мати такі властивості:
-                    - вміння конструювати квиток за номером;
-                    - можливість запитати ціну квитка;
-                    - можливість друку квитка у вигляді рядка.
-                    
-        Для останньої задачі реалізувати серіалізацію / десеріалізацію у форматі json проданих квитків.
-"""
+print('Генератор чисел в кубі:\n   ', *numbers_list(10))
