9c08154fe35156b25cd05d55a8805fba524d6a08
Dgikar <dgikar@ukr.net>
2023-02-07 14:50:13 +0200

    Python Django - Lesson - 05 - Homeworks

--- 03 - Python - Django/Python Django - Lesson - 05/Python Django - Homeworks/Python Django - Lesson - 05 - Homeworks.py
+++ 03 - Python - Django/Python Django - Lesson - 05/Python Django - Homeworks/Python Django - Lesson - 05 - Homeworks.py
@@ -1,8 +1,10 @@
 """
 Useful Links:
     Документація django models - https://docs.djangoproject.com/en/4.1/topics/db/models
+    Интеграция wysiwyg редактора с Django. Обзор - https://habr.com/ru/company/htdt/blog/202782
+    django-tinymce - https://github.com/jazzband/django-tinymce
+    Как вывести на странице поле базы данных с HTML разметкой? - https://qna.habr.com/q/736619
 -------------------------------------------
 
 
 """
-
