ba6477e7e105a4753a8be924e867e6f049886f86
Dgikar <dgikar@ukr.net>
2023-02-26 17:40:03 +0200

    Проект "Restaurant"

--- 03 - Python - Django/myLearningDjangoProjects/01 - Restaurant/Restaurant/Restaurant/settings.py
+++ 03 - Python - Django/myLearningDjangoProjects/01 - Restaurant/Restaurant/Restaurant/settings.py
@@ -121,7 +121,6 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    "media/",
 ]
 
 # Default primary key field type (Тип поля первинного ключа за замовчуванням)
