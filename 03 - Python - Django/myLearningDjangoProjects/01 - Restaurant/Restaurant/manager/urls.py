95c91a56717612074b75379ddbcfc42bc80c7c38
Dgikar <dgikar@ukr.net>
2023-02-26 17:25:29 +0200

    Додаток "Manager"

---
+++ 03 - Python - Django/myLearningDjangoProjects/01 - Restaurant/Restaurant/manager/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import reservation_list, update_reservation, contact_list, update_contact, manager_page
+
+app_name = "manager"
+
+urlpatterns = [
+    path("reservations/", reservation_list, name="reservations_list"),
+    path("reservations/update/<int:pk>", update_reservation, name="update_reserve"),
+    path("contact/", contact_list, name="contact_list"),
+    path("contact/update/<int:pk>", update_contact, name="update_contact"),
+    path('', manager_page, name="manager_page")
+]
